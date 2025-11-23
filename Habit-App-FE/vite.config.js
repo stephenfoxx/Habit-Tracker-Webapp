373799f1fde513109a4a27a6ad6b4bd96d9e5b30
stephen ogwu <116175711+stephenfoxx@users.noreply.github.com>
2025-11-23 18:57:38 +0000

    adding allowedhosts to viteconfig.js

--- Habit-App-FE/vite.config.js
+++ Habit-App-FE/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // allow all hosts
+    allowedHosts: ["habit-tracker-webapp-11.onrender.com"],
+  },
+});
